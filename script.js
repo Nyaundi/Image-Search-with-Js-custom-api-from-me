5f2283c3b8b375a6d3aa6218099bd5f501a13973
Nyaundi <jamesmabuka260@gmail.com>
2023-09-12 15:42:09 +0300

    Styling is done bt side by side is not responsive

---
+++ script.js
@@ -0,0 +1,6 @@
+const accessKey = "XVS2iulDwigeH2_i_yEH9Ojs6GD6cdsEUfBf_yz4XGw";
+
+const formEl = document.querySelector('form');
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector('.search-results');
+const showMore = document.getElementById('show-mode-buton');
