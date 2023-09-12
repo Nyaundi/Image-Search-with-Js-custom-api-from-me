1c736b5a41e9b9cb25aee48fd0bccc08f7bba8fc
Nyaundi <jamesmabuka260@gmail.com>
2023-09-12 16:38:31 +0300

    moules

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const formEl = document.querySelector('form');
 const inputEl = document.getElementById("search-input");
 const searchResults = document.querySelector('.search-results');
 const showMore = document.getElementById('show-mode-buton');
+
+let inputData = ""
+
+let page = 1;
+
+async function searchImages(){
+    inputData = inputEl.ariaValueMax;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}$client_id=${accessKey}`
+
+    const response = await fetch(url)
+    const data =await response.json()
+
+    const results = data.results
+
+    if (page === 1){
+        searchResults.innerHTML = ""
+    }
+    results.map((result) => {
+        const imageWraper = document.createElement('div');
+        imageWraper.classList.add("search-result")
+    })
+}
